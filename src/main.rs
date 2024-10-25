5f285d1113a9a11521305904440a7e49ce812f9d
Jesse Braham <jessebraham@users.noreply.github.com>
2024-10-25 04:05:05 -0700

    Add an `xtask` package to simplify our CI workflow (#22)
    * Add an option to specify the output directory of the generated project
    
    * Remove the `stack-protector` option
    
    * Add an `xtask` package to simplify our CI workflow
    
    * Update the CI workflow to use `xtask` package and run tests
    
    * Remove extraneous HAL initialization from `async_main`
    
    * Use `stable` release channel for running tests
    
    * Fix a typo in `README.md`

--- src/main.rs
+++ src/main.rs
@@ -104,13 +104,6 @@ static OPTIONS: &[GeneratorOptionItem] = &[
         disables: &[],
         chips: &[],
     }),
-    GeneratorOptionItem::Option(GeneratorOption {
-        name: "stack-protector",
-        display_name: "Enable stack-smash protection (`nightly` only).",
-        enables: &[],
-        disables: &[],
-        chips: &[],
-    }),
     GeneratorOptionItem::Category(GeneratorOptionCategory {
         name: "optional",
         display_name: "Options",
@@ -175,12 +168,25 @@ struct Args {
 
     #[arg(short, long)]
     option: Vec<String>,
+
+    #[arg(short = 'O', long)]
+    output_path: Option<PathBuf>,
 }
 
 fn main() {
     let args = Args::parse();
 
-    if env::current_dir().unwrap().join(&args.name).exists() {
+    let path = &args
+        .output_path
+        .clone()
+        .unwrap_or_else(|| env::current_dir().unwrap());
+
+    if !path.is_dir() {
+        eprintln!("Output path must be a directory");
+        process::exit(-1);
+    }
+
+    if path.join(&args.name).exists() {
         eprintln!("Directory already exists");
         process::exit(-1);
     }
@@ -190,6 +196,7 @@ fn main() {
 
     let mut selected = if !args.headless {
         let repository = tui::Repository::new(args.chip, OPTIONS, &args.option);
+
         // TUI stuff ahead
         let terminal = tui::init_terminal().unwrap();
 
@@ -227,7 +234,7 @@ fn main() {
         }
     }
 
-    let dir = PathBuf::from(&args.name);
+    let dir = path.join(&args.name);
     std::fs::create_dir(&dir).unwrap();
 
     for &(file_path, contents) in template_files::TEMPLATE_FILES.iter() {
@@ -248,14 +255,14 @@ fn main() {
         .arg("group_imports=StdExternalCrate")
         .arg("--config")
         .arg("imports_granularity=Module")
-        .current_dir(&args.name)
+        .current_dir(path.join(&args.name))
         .output()
         .unwrap();
 
     // Run git init
     process::Command::new("git")
         .arg("init")
-        .current_dir(&args.name)
+        .current_dir(path.join(&args.name))
         .output()
         .unwrap();
 }

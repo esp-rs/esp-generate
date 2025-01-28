f02b0c17bbb447f9ea6a45c76e8bd938d7439ccf
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-01-28 11:38:01 +0100

    Prevent problems with flip-link

--- template/build.rs
+++ template/build.rs
@@ -1,6 +1,7 @@
 fn main() {
-    println!("cargo:rustc-link-arg=-Tlinkall.x");
     //IF option("probe-rs")
     println!("cargo:rustc-link-arg=-Tdefmt.x");
     //ENDIF
+    // make sure linkall.x is the last linker script (otherwise might cause problems with flip-link)
+    println!("cargo:rustc-link-arg=-Tlinkall.x");
 }

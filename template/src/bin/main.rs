0fbbbfa71dc77e3818a21786caa310e5327691de
Juraj Sadel <juraj.sadel@espressif.com>
2025-06-03 17:00:01 +0200

    Add even better link to esp-idf docs about app desc

--- template/src/bin/main.rs
+++ template/src/bin/main.rs
@@ -42,8 +42,8 @@ use esp_backtrace as _;
 extern crate alloc;
 //ENDIF
 
-// This creates a default app-descriptor read by the esp-idf bootloader.
-// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/bootloader_image_format.html>
+// This creates a default app-descriptor required by the esp-idf bootloader.
+// For more information see: <https://docs.espressif.com/projects/esp-idf/en/stable/esp32/api-reference/system/app_image_format.html#application-description>
 esp_bootloader_esp_idf::esp_app_desc!();
 
 #[main]

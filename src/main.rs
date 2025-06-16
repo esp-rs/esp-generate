99c4aa925b8a5b9f10784562b140e2185490ea77
Kirill Mikhailov <konnor1980@yandex.ru>
2025-06-16 14:08:20 +0200

    gtg

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,15 @@ use std::{
     sync::LazyLock,
 };
 
-use strum::IntoEnumIterator;
 use clap::Parser;
 use env_logger::{Builder, Env};
 use esp_generate::config::find_option;
 use esp_generate::config::{ActiveConfiguration, Relationships};
 use esp_generate::template::{GeneratorOptionItem, Template};
 use esp_metadata::Chip;
+use inquire::{Select, Text};
+use strum::IntoEnumIterator;
 use taplo::formatter::Options;
-use inquire::{Text, Select};
 
 mod check;
 mod template_files;
@@ -94,13 +94,17 @@ fn setup_args_interactive(args: &mut Args) -> Result<(), Box<dyn Error>> {
 
         let chip_index = Select::new("Select your target chip:", chip_names.clone())
             .prompt()
-            .map(|selected| chip_names.iter().position(|&name| name == selected).unwrap())?;
+            .map(|selected| {
+                chip_names
+                    .iter()
+                    .position(|&name| name == selected)
+                    .unwrap()
+            })?;
 
         args.chip = Some(chip_variants[chip_index].clone());
     }
 
     if args.name.is_none() {
-
         let project_name = Text::new("Enter project name:")
             .with_default("my-esp-project")
             .prompt()?;
@@ -568,11 +572,7 @@ fn process_options(template: &Template, args: &Args) {
             log::error!("Unknown option '{}'", option);
             success = false;
         } else if !option_found_for_chip {
-            log::error!(
-                "Option '{}' is not supported for chip {}",
-                option,
-                arg_chip
-            );
+            log::error!("Option '{}' is not supported for chip {}", option, arg_chip);
             success = false;
         }
     }

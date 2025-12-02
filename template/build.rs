f5f1f6c66f2c4ee6b6aa31259fe59b7c50e15300
Björn Quentin <bjoernQ@users.noreply.github.com>
2025-12-02 17:07:17 +0800

    More linker hints (#264)
    * Check more linker errors in build.rs
    
    * Fix
    
    * Use common prefixes for hinting on linker errors where possible
    
    * Use correct identifier

--- template/build.rs
+++ template/build.rs
@@ -18,9 +18,11 @@ fn linker_be_nice() {
 
         match kind.as_str() {
             "undefined-symbol" => match what.as_str() {
-                "_defmt_timestamp" => {
+                what if what.starts_with("_defmt_") => {
                     eprintln!();
-                    eprintln!("💡 `defmt` not found - make sure `defmt.x` is added as a linker script and you have included `use defmt_rtt as _;`");
+                    eprintln!(
+                        "💡 `defmt` not found - make sure `defmt.x` is added as a linker script and you have included `use defmt_rtt as _;`"
+                    );
                     eprintln!();
                 }
                 "_stack_start" => {
@@ -28,16 +30,32 @@ fn linker_be_nice() {
                     eprintln!("💡 Is the linker script `linkall.x` missing?");
                     eprintln!();
                 }
-                "esp_rtos_initialized"
-                | "esp_rtos_yield_task"
-                | "esp_rtos_task_create" => {
+                what if what.starts_with("esp_rtos_") => {
                     eprintln!();
-                    eprintln!("💡 `esp-radio` has no scheduler enabled. Make sure you have initialized `esp-rtos` or provided an external scheduler.");
+                    eprintln!(
+                        "💡 `esp-radio` has no scheduler enabled. Make sure you have initialized `esp-rtos` or provided an external scheduler."
+                    );
                     eprintln!();
                 }
                 "embedded_test_linker_file_not_added_to_rustflags" => {
                     eprintln!();
-                    eprintln!("💡 `embedded-test` not found - make sure `embedded-test.x` is added as a linker script for tests");
+                    eprintln!(
+                        "💡 `embedded-test` not found - make sure `embedded-test.x` is added as a linker script for tests"
+                    );
+                    eprintln!();
+                }
+                "free"
+                | "malloc"
+                | "calloc"
+                | "get_free_internal_heap_size"
+                | "malloc_internal"
+                | "realloc_internal"
+                | "calloc_internal"
+                | "free_internal" => {
+                    eprintln!();
+                    eprintln!(
+                        "💡 Did you forget the `esp-alloc` dependency or didn't enable the `compat` feature on it?"
+                    );
                     eprintln!();
                 }
                 _ => (),

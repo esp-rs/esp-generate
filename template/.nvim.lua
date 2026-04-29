2822b0c532fddf0e30a37d5f4ada2bb745410616
Bowen Ho <bowen951209@gmail.com>
2026-04-29 08:42:55 +0800

    Fix how the stable toolchain is specified in Neovim
    - Remove the `rust_analyzer.server` configuration and replace
      it with a cmd field in `vim.lsp.config`, following the solution in #324.
    - Move `rust_analyzer.cargo.extraEnv` to improve readability.

--- template/.nvim.lua
+++ template/.nvim.lua
@@ -5,19 +5,22 @@ local rust_analyzer = {
         --REPLACE riscv32imac-unknown-none-elf rust_target
         target = "riscv32imac-unknown-none-elf",
         allTargets = false,
+        --IF option("xtensa")
+        --REPLACE esp rust_toolchain
+        extraEnv = { RUSTUP_TOOLCHAIN = "esp" },
+        --ENDIF
     },
 }
---IF option("xtensa")
---REPLACE esp rust_toolchain
-rust_analyzer.cargo.extraEnv = { RUSTUP_TOOLCHAIN = "esp" }
-rust_analyzer.server = { extraEnv = { RUSTUP_TOOLCHAIN = "stable" } }
---ENDIF
 
 --IF option("neovim-rustaceanvim")
+-- Note the rustaceanvim name of the language server is rust-analyzer with a dash.
 vim.lsp.config("rust-analyzer", {
 --ELSE
 -- Note the neovim name of the language server is rust_analyzer with an underscore.
 vim.lsp.config("rust_analyzer", {
+--ENDIF
+--IF option("xtensa")
+	cmd = { "rustup", "run", "stable", "rust-analyzer" },
 --ENDIF
     settings = {
         ["rust-analyzer"] = rust_analyzer,
